44192b33769e7efadb3d1519f87fa35e36dea379
James Durham <james.durham@vbrick.com>
2015-03-09 10:44:28 -0400

    allow preview to be turned off completely

--- insightDirective.js
+++ insightDirective.js
@@ -1,7 +1,6 @@
 module.exports = function insightDirective () {
 	return {
 		restrict: 'A',
-		require: '?ngModel',
 		templateUrl: function(elem, attr) {
 			return attr.templateUrl || 'insight.html';
 		},
@@ -9,133 +8,144 @@ module.exports = function insightDirective () {
 			insight: '='
 		},
 		replace: true,
-		link: function ($scope, element, attrs, ngModelCtrl) { //todo $scope -> scope
+		controller: ['$scope', insightController],
+		compile: function () {
+		}
+	};
+}
 
-			// this needs integration with list directive, infinite scroll, and search
+function insightController($scope){
 
-			$scope.state = {};
-			$scope.state.preview = 1;
+	// this needs integration with list directive, infinite scroll, and search
 
-			var data = $scope.insight.data;
-			$scope.favorites = [];
-			$scope.recents = [];
-			$scope.assignedItems = [];
+	$scope.state = {};
+	$scope.state.preview = {
+		usePreview: false,
+		showPreview: true
+	};
 
-			if (ngModelCtrl) {
-				ngModelCtrl.$render = function () {
-					_.each(ngModelCtrl.$modelValue, toggleItem);
-				};
-			}
+	var data = $scope.insight.data;
+	$scope.favorites = [];
+	$scope.recents = [];
 
-			$scope.addOption = function(item) {
+	$scope.favorites.push(data[3]);
+	$scope.favorites.push(data[6]);
+	$scope.favorites.push(data[7]);
 
-				if (!item.assigned) {
-					item.assigned = true;
-					item.selected = false;
+	$scope.recents.push(data[1]);
+	$scope.recents.push(data[2]);
+	$scope.recents.push(data[8]);
 
-					_.each($scope.assignedItems, function(group){ group.selected = false;});
 
-					$scope.assignedItems.push(item);
-					tryUpdateModel();
-				}
-			};
 
-			var toggleItem = $scope.toggleItem = function(item) {
+	$scope.assignedItems = [];
 
-				if (!item.assigned) {
-					$scope.showMessage = false;
-					$scope.currentSelection = item;
-					item.assigned = true;
-					$scope.assignedItems.push(item);
-					tryUpdateModel();
-				} else {
+	$scope.addOption = function(item) {
 
-					$scope.removeItem(item);
-				}
-			};
+		if (!item.assigned) {
+			item.assigned = true;
+			item.selected = false;
 
-			$scope.change = function(item){
+			_.each($scope.assignedItems, function(group){ group.selected = false;});
 
-				$scope.currentSelection = item;
-
-				if(item.assigned === false) {
-					$scope.showMessage = true;
-					$scope.messageItem = item;
-				} else {
-					$scope.showMessage = false;
-				}
-			};
-
-			$scope.selectItem = function(item){
-
-				_.each($scope.data, function(group){ group.selected = false;});
-				item.selected = true;
-			};
-
-			$scope.inputPlaceholder = 'Add groups ...';
-
-			$scope.setPlaceholder = function(value){
-				$scope.inputPlaceholder = value;
-				if(value === 'Find assigned groups ...'){
-					$scope.showOptions = false;
-				}
-			};
-
-			$scope.selectAndToggle = function(item) {
-				$scope.selection(item);
-				$scope.toggleItem(item);
-			};
-
-			$scope.selection = function(item){
-
-				$scope.showMessage = null;
-
-				if (item.selected) {
-					$scope.currentSelection = item;
-				} else {
-					$scope.currentSelection = null;
-				}
-			};
-
-			$scope.closeOptions = function() {
-				$scope.showOptions = false;
-				$scope.query = '';
-				$scope.focus = false;
-				$scope.currentSelection.selected = true;
-				// _.each($scope.data, function(group){ group.selected = false;});
-			};
-
-			$scope.removeItem = function(group) {
-				group.selected = false;
-				group.assigned = false;
-				$scope.showMessage = true;
-				$scope.messageItem = group;
-
-				var index = $scope.assignedItems.indexOf(group);
-				$scope.assignedItems.splice(index,1);
-				tryUpdateModel();
-			};
-
-			$scope.groupOptions = {};
-
-			$scope.groupOptions.displayItems = [
-				{id:0, field:'name', label:'name', fill:true},
-				{id:1, field:'users', label:'users', type:'array-length' },
-				{id:2, field:'status', label:'status' },
-				{id:3, field:null, label:'tools', type:'tools', actions: [
-					{label:'Edit'},
-					{label: 'Delete'}
-				]}
-			];
-
-			function tryUpdateModel() {
-				if (!ngModelCtrl) {
-					return;
-				}
-
-				ngModelCtrl.$setViewValue($scope.assignedItems);
-			}
+			$scope.assignedItems.push(item);
 		}
 	};
 
+	$scope.toggleItem = function(item) {
+
+		if (!item.assigned) {
+			$scope.showMessage = false;
+			$scope.currentSelection = item;
+			item.assigned = true;
+			$scope.assignedItems.push(item);
+		} else {
+
+			$scope.removeItem(item);
+		}
+	};
+
+	$scope.change = function(item){
+
+		$scope.currentSelection = item;
+
+		if(item.assigned === false) {
+			$scope.showMessage = true;
+			$scope.messageItem = item;
+		} else {
+			$scope.showMessage = false;
+		}
+	};
+
+	$scope.selectItem = function(item){
+
+		_.each($scope.data, function(group){ group.selected = false;});
+		item.selected = true;
+	};
+
+	$scope.inputPlaceholder = 'Add groups ...';
+
+	$scope.setPlaceholder = function(value){
+		$scope.inputPlaceholder = value;
+		if(value === 'Find assigned groups ...'){
+			$scope.showOptions = false;
+		}
+	};
+
+	$scope.selectAndToggle = function(item) {
+		$scope.selection(item);
+		$scope.toggleItem(item);
+	};
+
+	$scope.selection = function(item){
+
+		$scope.showMessage = null;
+
+		if (item.selected) {
+			$scope.currentSelection = item;
+		} else {
+			$scope.currentSelection = null;
+		}
+	};
+
+	$scope.closeOptions = function() {
+		$scope.showOptions = false;
+		$scope.query = '';
+		$scope.focus = false;
+		$scope.currentSelection.selected = true;
+		// _.each($scope.data, function(group){ group.selected = false;});
+	};
+
+	$scope.removeItem = function(group) {
+		group.selected = false;
+		group.assigned = false;
+		$scope.showMessage = true;
+		$scope.messageItem = group;
+
+		var found, index;
+
+		found =  $scope.assignedItems.filter(function(obj) {
+			return obj._id === group._id;
+		});
+
+		index = $scope.assignedItems.indexOf(found[0]);
+		$scope.assignedItems.splice(index,1);
+	};
+
+	$scope.groupOptions = {};
+
+	$scope.groupOptions.displayItems = [
+		{id:0, field:'name', label:'name', fill:true},
+		{id:1, field:'users', label:'users', type:'array-length' },
+		{id:2, field:'status', label:'status' },
+		{id:3, field:null, label:'tools', type:'tools', actions: [
+			{label:'Edit'},
+			{label: 'Delete'}
+		]}
+	];
+
+	/*$scope.groupOptions.actions = [
+		{label:'Edit', callback: GroupList.get},
+		{label: 'Delete', callback: GroupList.removeGroup}
+	];*/
 }
