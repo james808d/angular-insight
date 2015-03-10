b7692811c82d8cc5837ee5c75226599f45f9b3a4
Evan Oxfeld <eoxfeld@gmail.com>
2015-03-10 13:44:34 -0400

    Prepare example page for multiple examples

--- examples/controller.js
+++ examples/controller.js
@@ -2,5 +2,5 @@ module.exports = function($scope, GroupList) {
 	$scope.insightOptions = {
 		data: GroupList.groups
 	};
-	$scope.selectedGroups = [GroupList.groups[1], GroupList.groups[2]];
+	$scope.assignedGroups = [GroupList.groups[1], GroupList.groups[2]];
 }
