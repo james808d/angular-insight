e0e7e41e1ca9e1ff6664f108a2a7c558c0e2df85
Evan Oxfeld <eoxfeld@gmail.com>
2015-04-24 10:14:54 -0400

    Infinitely scroll assigned items

--- examples/controller.js
+++ examples/controller.js
@@ -1,6 +1,8 @@
 var _ = require('underscore');
 
-module.exports = function($scope, GroupList, MovieService) {
+module.exports = function($scope, $q, $timeout, GroupList, MovieService) {
+	var initialAssignedMovies = ['770946423', '714976247', '770800493', '771217285', '771311818', '771313962', '770955057', '770739679', '771312513', '770783549', '771247277'];
+
 	_.extend($scope, {
 		basicOptions: {
 			data: GroupList.groups,
@@ -17,7 +19,7 @@ module.exports = function($scope, GroupList, MovieService) {
 		},
 
 		movieOptions: {
-			data: [{ id: '770946423', title: 'Avengers', type: 'film' }],
+			data: [],
 			fieldDefs: {
 				identifier: 'id',
 				display: 'title',
@@ -26,7 +28,7 @@ module.exports = function($scope, GroupList, MovieService) {
 			dataTypes: {
 				film: 'film'
 			},
-			loadPage: function (query) {
+			loadQueryPage: function (query) {
 				if (!query.length) {
 					return
 				}
@@ -41,9 +43,35 @@ module.exports = function($scope, GroupList, MovieService) {
 
 						return movies
 					});
+			},
+			onAssignedInfiniteScroll: function loadNextTenMovies () {
+				var movies = [];
+				var movieIds = _.chain($scope.assignedMovies)
+							.filter(function (movie) { return !movie.title })
+							.first(10)
+							.pluck('id')
+							.value();
+
+				return movieIds
+					.map(function (id) { return _.partial(getMovie, id); })
+					.reduce($q.when, $q.when(true))
+					.then(function () {
+						return movies;
+					});
+
+				function getMovie (id) {
+					return $timeout(function() {
+						return MovieService
+							.getMovie(id)
+							.then(function (movie) {
+								movie.type = 'film';
+								movies.push(movie);
+							});
+					}, 500);
+				}
 			}
 		},
-		assignedMovies: [{ id: '770946423', type: 'film' }]
+		assignedMovies: initialAssignedMovies.map(function(id) { return { id: id }; })
 
 	});
 
--- examples/movieService.js
+++ examples/movieService.js
@@ -1,3 +1,5 @@
+var APIKEY = '9x4ypv6dgf2f5ptwn9u3k8s6';
+
 module.exports = function ($http) {
 	return {
 		queryMovies: function (query) {
@@ -7,11 +9,24 @@ module.exports = function ($http) {
 						callback: 'JSON_CALLBACK',
 						q: query,
 						page_limit: 10,
-						apikey: '9x4ypv6dgf2f5ptwn9u3k8s6'
+						apikey: APIKEY
 					}
 				})
 				.then(function (response) {
 					return response.data.movies;
+				});
+		},
+		getMovie: function (id) {
+			return $http
+				.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/' + id + '.json', {
+					params: {
+						callback: 'JSON_CALLBACK',
+						apikey: APIKEY
+					}
+				})
+				.then(function (response) {
+					response.data.id = id.toString();
+					return response.data;
 				});
 		}
 	};
